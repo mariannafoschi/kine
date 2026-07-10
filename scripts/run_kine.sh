160a724ff76e00b2e8e6a58574c9d8a434cd193e
mariannafoschi <foschimarianna@gmail.com>
2026-07-10 12:45:33 -0700

    added server specific tips

--- scripts/run_kine.sh
+++ scripts/run_kine.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# This export reserves for kine computations 90 percent of available GPU space
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.9
-
-# taskset --cpu-list 0 is only required to restrict the CPU core.
-# You may not need it.
-
 # Single dataset example
-taskset --cpu-list 0 python example_static_imaging.py -obs ../data/eht_m87_2017-04-10.uvfits -yml ../parameters/params_static_imaging.yml
+python example_static_imaging.py -obs ../data/eht_m87_2017-04-10.uvfits -yml ../parameters/params_static_imaging.yml
 
 # Multiple datasets example
-taskset --cpu-list 0 python example_multiepoch_imaging.py -obs ../data/ -yml ../parameters/params_multiepoch_imaging.yml
+python example_multiepoch_imaging.py -obs ../data/ -yml ../parameters/params_multiepoch_imaging.yml
