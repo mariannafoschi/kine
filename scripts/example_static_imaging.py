f770fcbdbec5dd48b0f3f001319b9096e23edfe7
mariannafoschi <foschimarianna@gmail.com>
2026-07-09 19:04:35 -0700

    author's updates

--- scripts/example_static_imaging.py
+++ scripts/example_static_imaging.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2026 Marianna Foschi
+# Copyright (C) 2026 Marianna Foschi, Antonio Fuentes, Brandon Zhao
 
 # This program is free software: you can redistribute it and/or modify it under 
 # the terms of the GNU General Public License as published by the Free Software 
 # Foundation, either version 3 of the License, or (at your option) any later 
-#version.
+# version.
 
 # This program is distributed in the hope that it will be useful, but WITHOUT 
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
--- scripts/run_kine.sh
+++ scripts/run_kine.sh
@@ -5,4 +5,9 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.9
 
 # taskset --cpu-list 0 is only required to restrict the CPU core.
 # You may not need it.
-taskset --cpu-list 0 python dynamic_imaging_example.py -obs ../data/some_dataset.uvfits -yml ./dynamic_imaging_params.yml
+
+# Single dataset example
+taskset --cpu-list 0 python example_static_imaging.py -obs ../data/eht_m87_2017-04-10.uvfits -yml ../parameters/params_static_imaging.yml
+
+# Multiple datasets example
+taskset --cpu-list 0 python example_multiepoch_imaging.py -obs ../data/ -yml ../parameters/params_multiepoch_imaging.yml
