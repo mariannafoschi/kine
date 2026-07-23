9ce4abc4842d39a43ecf8a91dbf3f80c8bb71539
mariannafoschi <foschimarianna@gmail.com>
2026-07-22 20:59:04 -0700

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ html_theme_options = {
     'repository_url': 'https://github.com/mariannafoschi/kine',
     'use_repository_button': True,
     'use_source_button': True,
-    'header_tabs': True,
+    'header_tabs': False,
 }
 
 # -- Options for EPUB output
